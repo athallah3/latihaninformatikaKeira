9a928c0cf74b3ff941d40279bec6c0b87364555f
athallah3 <keiramzyathallah@gmail.com>
2025-05-27 09:44:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,12 @@ st.write(
     "Running on caffeine and confusion [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.image("IMG_1889.jpeg", width=200)
+
+st.title("Code Hard, Cry Harder")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("write a number:", value=0, step=1)
+
+if(angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
